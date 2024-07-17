cce290fb577d3ba2b5665c9564c422d43a1fc24d
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-07-17 15:45:46 +0300

    Update Progression.java
    add Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,28 +1,39 @@
 package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
+import java.util.Arrays;
 
 public class Progression {
-    public static void run() {
+   public static void run() {
         var questions = new String [3][];
         for (int i = 0; i < 3; i++) {
             questions[i]= generateRound ();
         }
-        Engine.metod (questions, "Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+        Engine.metod (questions, "What number is missing in the progression?");
 
     }
     public static String [] generateRound(){
-        int number = Utils.getRandomint (1,10);
-        var correctAnswer = isSimple (number) ? "yes":"no";
-        return new String[]{String.valueOf (number), String.valueOf (correctAnswer)};
+        int number = Utils.getRandomint (1,4);
+        String array  = getStringMas();
+        String[] words = array.split(",");
+        String c = words[number];
+        words[number]="..";
+
+        return new String[]{String.valueOf (Arrays.toString (words)), String.valueOf (c)};
     }
-    public static boolean isSimple(int number) {
-        if(number < 2) return false;
-        for(int i = 2; i <= number / 2; i++) {
-            if(number % i == 0) {
-                return false;
-            }
-        }
-        return true;
+public static String getStringMas (){
+    int a = Utils.getRandomint (1,3);
+    String mas = "";
+    if (a==1){
+        mas = "2,4,6,8,10";
+
+    } else if (a==2) {
+        mas = "4,6,8,10,12";
+
+    } else if (a==3) {
+        mas = "3,6,9,12,15";
+
     }
+    return mas;
+}
 }
