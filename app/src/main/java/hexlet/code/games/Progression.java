a00424b8438b377c67cc1d7896ddba042c73515f
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-02 22:24:35 +0300

    Update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -8,27 +8,26 @@ public class Progression {
     public static void run() {
         var questions = new String [3][];
         for (int i = 0; i < 3; i++) {
-            questions[i]= generateRound ();
+            questions[i]= generateRound();
         }
         Engine.metod (questions, "What number is missing in the progression?");
-
     }
 
-   public static String [] generateRound(){
-        int number = Utils.getRandomint (1,4);
-        int sequenceSize = Utils.getRandomint (5,10); //размер последовательности
-        int firstNumber = 1; // первое число
-        int step = 5; // шаг
+   public static String [] generateRound() {
+        int number = Utils.getRandomint(1,4);
+        int sequenceSize = Utils.getRandomint(5, 10);
+        int firstNumber = 1;
+        int step = 5;
         String[] words = getStringMas(sequenceSize, firstNumber, step);
         String c = words[number];
-        words[number]="..";
-        return new String[]{String.join (" ",words), String.valueOf (c)};
+        words[number] = "..";
+        return new String[] {String.join(" ", words), String.valueOf(c)};
     }
-    
-public static String[] getStringMas (int sequenceSize, int firstNumber, int step){
+
+public static String[] getStringMas(int sequenceSize, int firstNumber, int step) {
     String[] array = new String[sequenceSize];
         for (int i = 0; i < array.length; i++) {
-        array[i] = Integer.toString (firstNumber+step*i);
+        array[i] = Integer.toString(firstNumber+step*i);
         }
         return array;
     }
