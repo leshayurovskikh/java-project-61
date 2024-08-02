af7b58690824476a3855c4fe2ab7663c085072e3
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-02 22:19:37 +0300

    Update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -19,8 +19,10 @@ public class Prime {
     }
 
    public static boolean isSimple(int number) {
-        if (number < 2) return false;
-        for (int i = 2; i <= Math.sqrt (number); i++) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 return false;
             }
