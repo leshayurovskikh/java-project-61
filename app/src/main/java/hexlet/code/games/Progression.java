e106a8e9e4ff37b52fe56a37b827cb9492675a80
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-02 23:42:47 +0300

    Update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -3,17 +3,17 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Progression {
-public static final int NUMBER_OF_ROUNDS = 3;
-public static final int FIRST_NUMBER = 1;  
-public static final int STEP = 5;    
-public static final int SEQ_MIN = 5; 
-public static final int SEQ_MAX = 10;
+public static final int NUMBEROFROUNDS = 3;
+public static final int FIRSTNUMBER = 1;
+public static final int STEP = 5;
+public static final int SEQMIN = 5;
+public static final int SEQMAX = 10;
 public static final int MAX = 5;
-public static final int MIN = 1;    
-    
+public static final int MIN = 1;
+
     public static void run() {
-        var questions = new String [NUMBER_OF_ROUNDS][];
-        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
+        var questions = new String[NUMBEROFROUNDS][];
+        for (int i = 0; i < NUMBEROFROUNDS; i++) {
             questions[i] = generateRound();
         }
         Engine.metod(questions, "What number is missing in the progression?");
@@ -21,17 +21,17 @@ public static final int MIN = 1;
 
    public static String[] generateRound() {
         int number = Utils.getRandomint(MIN, MAX);
-        int sequenceSize = Utils.getRandomint(SEQ_MIN, SEQ_MAX);
-        String[] words = getStringMas(sequenceSize, FIRST_NUMBER, STEP);
+        int sequenceSize = Utils.getRandomint(SEQMIN, SEQMAX);
+        String[] words = getStringMas(sequenceSize, FIRSTNUMBER, STEP);
         String c = words[number];
         words[number] = "..";
         return new String[] {String.join(" ", words), String.valueOf(c)};
     }
 
-public static String[] getStringMas(int sequenceSize, int FIRST_NUMBER, int STEP) {
+public static String[] getStringMas(int sequenceSize, int FIRSTNUMBER, int STEP) {
     String[] array = new String[sequenceSize];
         for (int i = 0; i < array.length; i++) {
-        array[i] = Integer.toString(FIRST_NUMBER + STEP * i);
+        array[i] = Integer.toString(FIRSTNUMBER + STEP * i);
         }
         return array;
     }
