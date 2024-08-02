f45ae9a5aef55670d941c55e975b1612847b969a
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-02 22:35:38 +0300

    Update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,19 +1,18 @@
 package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
-import java.util.Arrays;
 
 public class Progression {
 
     public static void run() {
         var questions = new String [3][];
         for (int i = 0; i < 3; i++) {
-            questions[i]= generateRound();
+            questions[i] = generateRound();
         }
-        Engine.metod (questions, "What number is missing in the progression?");
+        Engine.metod(questions, "What number is missing in the progression?");
     }
 
-   public static String [] generateRound() {
+   public static String[] generateRound() {
         int number = Utils.getRandomint(1,4);
         int sequenceSize = Utils.getRandomint(5, 10);
         int firstNumber = 1;
@@ -27,7 +26,7 @@ public class Progression {
 public static String[] getStringMas(int sequenceSize, int firstNumber, int step) {
     String[] array = new String[sequenceSize];
         for (int i = 0; i < array.length; i++) {
-        array[i] = Integer.toString(firstNumber+step*i);
+        array[i] = Integer.toString(firstNumber + step * i);
         }
         return array;
     }
