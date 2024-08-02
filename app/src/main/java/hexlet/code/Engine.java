ca987556062468e4a0e6fe1de522bfebe14f97db
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-02 21:56:06 +0300

    Update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,32 +5,32 @@ public class Engine {
     public static void correctAnswer() {
         System.out.println("Correct!");
     }
-    
-public static void wrongAnswer(String userName, String correctAnswer, String userAnswer){
+
+public static void wrongAnswer(String userName, String correctAnswer, String userAnswer) {
     System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was " + "'" + correctAnswer + "'" + ".");
     System.out.println("Let's try again, " + userName + "!");
     }
-    
+
 public static String getUserName() {
     System.out.println("Welcome to the Brain Games!");
     System.out.println("May I have your name?");
-    Scanner scanner = new Scanner (System.in);
+    Scanner scanner = new Scanner(System.in);
     String userName = scanner.next();
     System.out.println("Hello, " + userName + "!");
     return userName;
     }
-    
+
 public static void metod(String[][]rounds, String rules) {
     Scanner scanner = new Scanner(System.in);
     String userName = getUserName();
     System.out.println(rules);
-    for(String[] round:rounds){
-        System.out.println("Question: " + round[0] );
+    for (String[] round:rounds) {
+        System.out.println("Question: " + round[0]);
         String answer = scanner.next();
-        if(answer.equals(round[1])){
+        if (answer.equals(round[1])) {
             correctAnswer();
-            }else{
-            wrongAnswer(userName,String.valueOf(round[1]),answer );
+            } else {
+            wrongAnswer(userName, String.valueOf(round[1]), answer);
             return;
             }
         }
