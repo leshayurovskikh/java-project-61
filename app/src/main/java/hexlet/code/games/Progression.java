5971d6bfc7021ff4a65a200f69f4cc6be3be316e
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-02 23:25:15 +0300

    Update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -3,30 +3,35 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Progression {
-
+public static final int NUMBER_OF_ROUNDS = 3;
+public static final int FIRST_NUMBER = 1;  
+public static final int STEP = 5;    
+public static final int SEQ_MIN = 5; 
+public static final int SEQ_MAX = 5;
+public static final int MAX = 5;
+public static final int MIN = 5;    
+    
     public static void run() {
-        var questions = new String [3][];
-        for (int i = 0; i < 3; i++) {
+        var questions = new String [NUMBER_OF_ROUNDS][];
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
             questions[i] = generateRound();
         }
         Engine.metod(questions, "What number is missing in the progression?");
     }
 
    public static String[] generateRound() {
-        int number = Utils.getRandomint(1,4);
-        int sequenceSize = Utils.getRandomint(5, 10);
-        int firstNumber = 1;
-        int step = 5;
-        String[] words = getStringMas(sequenceSize, firstNumber, step);
+        int number = Utils.getRandomint(MAX, MIN);
+        int sequenceSize = Utils.getRandomint(SEQ_MIN, SEQ_MAX);
+        String[] words = getStringMas(sequenceSize, FIRST_NUMBER, STEP);
         String c = words[number];
         words[number] = "..";
         return new String[] {String.join(" ", words), String.valueOf(c)};
     }
 
-public static String[] getStringMas(int sequenceSize, int firstNumber, int step) {
+public static String[] getStringMas(int sequenceSize, int FIRST_NUMBER, int STEP) {
     String[] array = new String[sequenceSize];
         for (int i = 0; i < array.length; i++) {
-        array[i] = Integer.toString(firstNumber + step * i);
+        array[i] = Integer.toString(FIRST_NUMBER + STEP * i);
         }
         return array;
     }
