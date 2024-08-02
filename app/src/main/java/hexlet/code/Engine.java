2011048fec9972f12fe124b83915393011aef1ab
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-02 21:35:58 +0300

    Update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,36 +4,36 @@ import java.util.Scanner;
 public class Engine {
     public static void correctAnswer() {
         System.out.println("Correct!");
-        }
+    }
 public static void wrongAnswer(String userName, String correctAnswer, String userAnswer){
     System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was " + "'" + correctAnswer + "'" + ".");
     System.out.println("Let's try again, " + userName + "!");
     }
     
-public static String getUserName(){
-    System.out.println ("Welcome to the Brain Games!");
-    System.out.println ("May I have your name?");
+public static String getUserName() {
+    System.out.println("Welcome to the Brain Games!");
+    System.out.println("May I have your name?");
     Scanner scanner = new Scanner (System.in);
-    String userName = scanner.next ();
-    System.out.println ("Hello, "+userName+"!");
+    String userName = scanner.next();
+    System.out.println("Hello, " + userName + "!");
     return userName;
     }
     
-public static void metod(String[][]rounds, String rules){
+public static void metod(String[][]rounds, String rules) {
     Scanner scanner = new Scanner(System.in);
     String userName = getUserName();
-    System.out.println (rules);
-    for (String[] round:rounds){
-        System.out.println ("Question: " + round[0] );
-        String answer = scanner.next ();
-        if (answer.equals (round[1])){
+    System.out.println(rules);
+    for(String[] round:rounds){
+        System.out.println("Question: " + round[0] );
+        String answer = scanner.next();
+        if(answer.equals(round[1])){
             correctAnswer();
-            }else {
-            wrongAnswer (userName,String.valueOf (round[1]),answer );
+            }else{
+            wrongAnswer(userName,String.valueOf(round[1]),answer );
             return;
             }
         }
-    System.out.println ("Congratulations, "+userName+"!" );
+    System.out.println("Congratulations, " + userName + "!");
     }
 }
 
