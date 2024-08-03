594fcb8f99c106af5fa483722058156acd439faa
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-03 14:08:05 +0300

    Update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -5,7 +5,7 @@ import hexlet.code.Utils;
 public class Progression {
 public static final int NUMBEROFROUNDS = 3;
 public static final int FIRSTNUMBER = 1;
-public static final int S = 5;
+public static final int step = 5;
 public static final int SEQMIN = 5;
 public static final int SEQMAX = 10;
 public static final int MAX = 5;
@@ -21,7 +21,7 @@ public static final int MIN = 1;
    public static String[] generateRound() {
         int number = Utils.getRandomint(MIN, MAX);
         int sequenceSize = Utils.getRandomint(SEQMIN, SEQMAX);
-        String[] words = getStringMas(sequenceSize, FIRSTNUMBER, S);
+        String[] words = getStringMas(sequenceSize, FIRSTNUMBER, step);
         String c = words[number];
         words[number] = "..";
         return new String[] {String.join(" ", words), String.valueOf(c)};
@@ -30,7 +30,7 @@ public static final int MIN = 1;
 public static String[] getStringMas(int sequenceSize, int FIRSTNUMBER, int S) {
     String[] array = new String[sequenceSize];
         for (int i = 0; i < array.length; i++) {
-        array[i] = Integer.toString(FIRSTNUMBER + S * i);
+        array[i] = Integer.toString(FIRSTNUMBER + step * i);
         }
         return array;
     }
