04fa7c2e94b5e2eb3d7e29f4ec364495af2f4d8d
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-03 15:31:51 +0300

    Update Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -2,8 +2,8 @@ package hexlet.code;
 
 public class Utils {
 
-        public static int getRandomint(int min, int max){
-            double a = (int) ( min + Math.random () * max);
+        public static int getRandomint(int min, int max) {
+            double a = (int) (min + Math.random() * max);
             return (int) a;
         }
 }
