7adb079ccbe8484f4b684fff508c35da1814c5c5
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-03 15:13:13 +0300

    Update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -3,19 +3,19 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Even {
-
+public static final int NUMBER_OF_ROUNDS = 3;
+public static final int MIN_NUMBER = 1;
+public static final int MAX_NUMBER = 10;
     public static void run() {
-        var questions = new String[3][];
-        for (int i = 0; i < 3; i++) {
+        var questions = new String[NUMBER_OF_ROUNDS][];
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
             questions[i] = generateRound();
         }
         Engine.metod(questions, "Answer 'yes' if the number is even, otherwise answer 'no'.");
     }
 
     public static String[] generateRound() {
-        int minNumber = 1;
-        int maxNamber = 10;
-        int number = Utils.getRandomint(minNumber, maxNamber);
+        int number = Utils.getRandomint(MIN_NUMBER, MAX_NUMBER);
         var correctAnswer = isEven(number) ? "yes" : "no";
         return new String[]{String.valueOf(number), String.valueOf(correctAnswer)};
     }
