75561392b47cdd5ddacee5aa97761b68901a26f6
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-03 11:59:15 +0300

    Update Gcd.java

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -3,18 +3,20 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Gcd {
-
+public static final int NUMBER_OF_ROUNDS = 3;
+public static final int MIN_NUMBER = 1;
+public static final int MAX_NUMBER = 10; 
     public static void run() {
-        var questions = new String[3][];
-        for (int i = 0; i < 3; i++) {
+        var questions = new String[NUMBER_OF_ROUNDS][];
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
             questions[i] = generateRound();
         }
         Engine.metod(questions, "Find the greatest common divisor of given numbers.");
     }
 
     public static String[] generateRound() {
-        int a = Utils.getRandomint(1, 10);
-        int b = Utils.getRandomint(1, 10);
+        int a = Utils.getRandomint(MIN_NUMBER, MAX_NUMBER);
+        int b = Utils.getRandomint(MIN_NUMBER, MAX_NUMBER);
         int gcd =  gcd(a, b);
         return new String[] {String.valueOf(a + " " + b), String.valueOf(gcd)};
     }
