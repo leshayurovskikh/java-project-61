8b4e1e2bbe1bc4cd73afa9d6824af62d6b7aff95
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-03 12:44:03 +0300

    Update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -5,9 +5,10 @@ import hexlet.code.Utils;
 public class Progression {
 public static final int NUMBEROFROUNDS = 3;
 public static final int FIRST_NUMBER = 1;
+public static final int STEP = 5;
 public static final int SEQMIN = 5;
 public static final int SEQMAX = 10;
-public static final int MAX = 10;
+public static final int MAX = 9;
 public static final int MIN = 1;
 
     public static void run() {
@@ -22,16 +23,16 @@ public static final int MIN = 1;
         int number = Utils.getRandomint(MIN, MAX);
         int step = 5;
         int sequenceSize = Utils.getRandomint(SEQMIN, SEQMAX);
-        String[] words = getStringMas(sequenceSize, FIRST_NUMBER, step);
+        String[] words = getStringMas(sequenceSize, FIRST_NUMBER, STEP);
         String c = words[number];
         words[number] = "..";
         return new String[] {String.join(" ", words), String.valueOf(c)};
     }
 
-public static String[] getStringMas(int sequenceSize, int FIRST_NUMBER, int step) {
+public static String[] getStringMas(int sequenceSize, int FIRST_NUMBER, int STEP) {
     String[] array = new String[sequenceSize];
         for (int i = 0; i < array.length; i++) {
-        array[i] = Integer.toString(FIRST_NUMBER + step * i);
+        array[i] = Integer.toString(FIRST_NUMBER + STEP * i);
         }
         return array;
     }
