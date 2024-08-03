c678072c9643543616e56b635d57cd96f52bd6ad
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-08-03 13:50:58 +0300

    Update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -5,12 +5,11 @@ import hexlet.code.Utils;
 public class Progression {
 public static final int NUMBEROFROUNDS = 3;
 public static final int FIRST_NUMBER = 1;
-public static final int S = 5;
+public static final int STEP = 5;
 public static final int SEQMIN = 5;
 public static final int SEQMAX = 10;
 public static final int MAX = 5;
 public static final int MIN = 1;
-
     public static void run() {
         var questions = new String[NUMBEROFROUNDS][];
         for (int i = 0; i < NUMBEROFROUNDS; i++) {
@@ -20,19 +19,18 @@ public static final int MIN = 1;
     }
 
    public static String[] generateRound() {
-        int number = Utils.getRandomint(SEQMIN, SEQMAX);
+        int number = Utils.getRandomint(MIN, MAX);
         int sequenceSize = Utils.getRandomint(SEQMIN, SEQMAX);
-        String[] words = getStringMas(sequenceSize, FIRST_NUMBER, S);
+        String[] words = getStringMas(sequenceSize, FIRST_NUMBER, STEP);
         String c = words[number];
         words[number] = "..";
         return new String[] {String.join(" ", words), String.valueOf(c)};
     }
 
-public static String[] getStringMas(int sequenceSize, int FIRST_NUMBER, int S) {
+public static String[] getStringMas(int sequenceSize, int FIRST_NUMBER, int STEP) {
     String[] array = new String[sequenceSize];
         for (int i = 0; i < array.length; i++) {
-        array[i] = Integer.toString(FIRST_NUMBER + S * i);
+        array[i] = Integer.toString(FIRST_NUMBER + STEP * i);
         }
         return array;
     }
-}
